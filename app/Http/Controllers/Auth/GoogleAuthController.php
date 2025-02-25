2f99cdbc03582f682b239a5b898b63195a5d8581
zahra boojar <zahraboojar866@gmail.com>
2025-02-25 16:13:42 +0330

    update google controller

--- app/Http/Controllers/Auth/GoogleAuthController.php
+++ app/Http/Controllers/Auth/GoogleAuthController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Models\User;
 use Laravel\Socialite\Facades\Socialite;
-use function PHPUnit\Framework\returnArgument;
 
 class GoogleAuthController extends Controller
 {
@@ -16,6 +15,19 @@ class GoogleAuthController extends Controller
 
     public function callback()
     {
-        return 'test';
+        $google_user = Socialite::driver('google')->user();
+        $user = User::where('email', $google_user->email)->first();
+
+        if($user) {
+            auth()->loginUsingId($user->id);
+        } else {
+            $new_user = User::create([
+                'name' => $google_user->name,
+                'email' => $google_user->email,
+                'password' => bcrypt(\Str::random(16))
+            ]);
+
+            auth()->loginUsingId($new_user->id);
+        }
     }
 }
