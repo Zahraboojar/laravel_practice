e60c024250702dbb03b11a7cd7810cc6d777f8be
zahra boojar <zahraboojar866@gmail.com>
2025-03-28 20:20:52 +0430

    nothing

--- database/migrations/2025_03_28_154307_create_categories_table.php
+++ database/migrations/2025_03_28_154307_create_categories_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamps();
         });
 
+        
         Schema::create('category_product' , function(Blueprint $table) {
             $table->unsignedBigInteger('category_id');
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
